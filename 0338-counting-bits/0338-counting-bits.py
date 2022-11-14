86c4d8abb017c7750d7f0eece09dda2fd681cbfa
Mohamed Yasser <94971921+MYElmasry@users.noreply.github.com>
2022-11-14 08:21:04 +0200

    Time: 618 ms (5.59%), Space: 20.8 MB (79.83%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,10 +2,12 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         arr = []
         res = [0]
+        def getSum(n):
+            sum = 0
+            for digit in str(n): 
+              sum += int(digit)      
+            return sum
         for i in range(1, n+1):
-            while i > 0:
-                arr.append(i % 2)
-                i = i // 2
-            res.append(sum(arr))
-            arr = []
+            binary = bin(i).replace("0b", "")
+            res.append(getSum(binary))
         return res
